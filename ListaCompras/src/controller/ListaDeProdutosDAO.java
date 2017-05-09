9fdb7b3e3e5321d55ed80d03a74c9b0f0b2cc9b7
luizamazo <luizamazo@hotmail.com>
2017-05-09 15:23:49 -0300

    Criei as classes DAO

---
+++ ListaCompras/src/controller/ListaDeProdutosDAO.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class ListaDeProdutosDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ListaCompras/src/controller/LocalDAO.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class LocalDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
