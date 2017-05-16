b0a017d1111b1d10061c1060e36362003b09980c
AlanLucasSC <a96654117>
2017-05-16 16:10:59 -0400

    Master e Usuario - Atualizados by Kelvyn

--- ListaCompras/src/módulos/Master.java
+++ ListaCompras/src/módulos/Master.java
@@ -1,10 +1,34 @@
 package módulos;
 
 public class Master {
-
-	/**
-	 * @param args
-	 */
 	
+	String senha;
+	String email;
+	int id_supermercado;
+	
+	public Master(String senha, String email, int id_supermercado) {
+	this.senha = senha;
+	this.email = email;
+	this.id_supermercado = id_supermercado;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getId_supermercado() {
+		return id_supermercado;
+	}
+	public void setId_supermercado(int id_supermercado) {
+		this.id_supermercado = id_supermercado;
+	}
 
 }
--- ListaCompras/src/módulos/Usuario.java
+++ ListaCompras/src/módulos/Usuario.java
@@ -1,11 +1,24 @@
 package módulos;
 
 public class Usuario {
-	private String Nome;
+	private String nome;
 	private int id;
-	/**
-	 * @param args
-	 */
-	
 
+	public Usuario(String nome, int id) {
+
+		this.nome = nome;
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
