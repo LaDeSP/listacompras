f3994a0db38501272f310534930c854575526c1a
AlanLucasSC <a96654117>
2017-05-16 16:33:25 -0400

    Local Atualizado

--- ListaCompras/src/módulos/Local.java
+++ ListaCompras/src/módulos/Local.java
@@ -1,10 +1,49 @@
 package módulos;
 
 public class Local {
-
-	/**
-	 * @param args
-	 */
+	private String CEP;
+	private int id;
+	private String Cidade;
+	private String Estado;
 	
+	public Local(String cEP, int id, String cidade, String estado) {
+		CEP = cEP;
+		this.id = id;
+		Cidade = cidade;
+		Estado = estado;
+	}
 
+	public String getCEP() {
+		return CEP;
+	}
+
+	public void setCEP(String cEP) {
+		CEP = cEP;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCidade() {
+		return Cidade;
+	}
+
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	
+	
 }
