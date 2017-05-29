1d9cf4c059cf65ae8e2d62b2fe2c8432076a10bb
krdg10 <krdg10@live.com>
2017-05-29 16:54:53 -0300

    a

---
+++ ListaCompras/src/view/ViewCadastrarPromocao.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Scanner;
+
+import módulos.Promocao;
+
+public class ViewCadastrarPromocao {
+	Scanner entrada = new Scanner(System.in);
+	
+	Promocao promocao;
+	public ViewCadastrarPromocao() {
+
+	}
+
+
+}
